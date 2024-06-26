288da1a85f4652dd0420e5dbe60ae808b531d9f6
Nuris Akbar <nuris.akbar@gmail.com>
2024-06-26 10:34:27 +0700

    add jurusan_id

---
+++ database/migrations/2024_06_26_025319_add_jurusan_id_to_student.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddJurusanIdToStudent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->integer('jurusan_id')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
+    }
+}
